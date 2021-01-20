44c7cbf1eede193e693c22216d3ca214679dace5
Andrew Piper <andrew@threax.com>
2021-01-20 08:53:58 -0500

    Updated app menu.

--- Threax.Keepass/Views/Shared/Header.ts
+++ Threax.Keepass/Views/Shared/Header.ts
@@ -1,29 +1,64 @@
 import * as controller from 'hr.controller';
 import * as startup from 'clientlibs.startup';
-import * as menu from 'hr.appmenu.AppMenu';
 import * as client from 'clientlibs.ServiceClient';
+import * as tm from 'hr.accesstoken.manager';
+import * as loginPopup from 'hr.relogin.LoginPopup';
+import * as safepost from 'hr.safepostmessage';
 
-class AppMenuInjector extends menu.AppMenuInjector<client.EntryPointResult> {
+const AccessTokenClaim = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
+
+class AppMenuController {
     public static get InjectorArgs(): controller.DiFunction<any>[] {
-        return [client.EntryPointInjector];
+        return [controller.BindingCollection, client.EntryPointInjector, tm.TokenManager];
     }
 
-    constructor(private entryPointInjector: client.EntryPointInjector) {
-        super();
+    private userInfoView: controller.IView<any>;
+    private loggedInAreaToggle: controller.OnOffToggle;
+    private adminToggle: controller.OnOffToggle;
+    private users: controller.OnOffToggle;
+
+    constructor(bindings: controller.BindingCollection,
+        private entryPointInjector: client.EntryPointInjector,
+        private tokenManger: tm.TokenManager,
+        private messageValidator: safepost.PostMessageValidator) {
+
+        this.userInfoView = bindings.getView("userInfo");
+        this.loggedInAreaToggle = bindings.getToggle("loggedInArea");
+        this.adminToggle = bindings.getToggle("admin");
+        this.users = bindings.getToggle("users");
+
+        //Listen for relogin events
+        window.addEventListener("message", e => { this.handleMessage(e); });
+
+        this.setup();
     }
 
-    public * createMenu(entry: client.EntryPointResult): Generator<menu.AppMenuItem> {
+    public async setup(): Promise<void> {
+        const entry = await this.entryPointInjector.load();
 
-        if (entry.canListUsers()) {
-            yield { text: "Users", href: "Admin/Users" };
+        let accessToken = await this.tokenManger.getAccessToken();
+        if (accessToken) {
+            this.userInfoView.setData({
+                userName: accessToken[AccessTokenClaim]
+            });
         }
+        this.loggedInAreaToggle.mode = accessToken !== null;
+
+        let showAdmin = false;
+        showAdmin = this.users.mode = entry.canListUsers() || showAdmin;
+        this.adminToggle.mode = showAdmin;
     }
 
-    public getEntryPoint(): Promise<client.EntryPointResult> {
-        return this.entryPointInjector.load();
+    private handleMessage(e: MessageEvent): void {
+        if (this.messageValidator.isValid(e)) {
+            const message: loginPopup.ILoginMessage = e.data;
+            if (message.type === loginPopup.MessageType && message.success) {
+                this.setup();
+            }
+        }
     }
 }
 
 const builder = startup.createBuilder();
-menu.addServices(builder.Services, AppMenuInjector);
-builder.create("appMenu", menu.AppMenu);
+builder.Services.addShared(AppMenuController, AppMenuController);
+builder.create("appMenu", AppMenuController);
